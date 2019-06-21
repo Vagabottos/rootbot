4d916528b4e9fb206ee55fbea0351299c83310dd
Kyle J. Kemp <kyle@seiyria.com>
2019-06-21 16:28:38 -0500

    add error logging

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -8,12 +8,34 @@ import { ICommandResult } from '../interfaces';
 @Singleton
 @AutoWired
 export class Logger extends BaseService {
+
+  public async init(client) {
+    super.init(client);
+
+    this.watchGlobalUncaughtExceptions();
+  }
+
   log(...args) {
-    console.log(new Date(), ...args);
+    console.log(this.timeStamp(), ...args);
   }
 
   logCommandResult(result: ICommandResult) {
     if (!result.result && !result.resultString) { return; }
     this.log(result);
+  }
+
+  error(...args) {
+    console.error(this.timeStamp(), ...args);
+  }
+
+  private timeStamp() {
+    return new Date();
+  }
+
+  private watchGlobalUncaughtExceptions() {
+    process.on('uncaughtException', (e) => {
+      this.error(e);
+      process.exit(0);
+    });
   }
 }
