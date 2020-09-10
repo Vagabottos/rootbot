c175b9fa7c2ce2480a5dc1fcc0c1961e6e712e59
seiyria <kyle@seiyria.com>
2020-09-10 13:16:06 -0500

    add better sorting for all cards on *cards sites

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,49 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-  const lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+const sets = new Set();
+window.__gamedata.forEach(card => (card.set || ['Unspecified']).forEach(s => sets.add(s)));
 
-  // lazy load images where possible
-  if ('IntersectionObserver' in window) {
-    const lazyImageObserver = new IntersectionObserver((entries, observer) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
+Vue.use(BootstrapVue);
+
+const app = new Vue({
+  el: '#app',
+
+  data: {
+    cards: window.__gamedata,
+    sets,
+    observer: null,
+    currentSet: ''
+  },
+  
+  methods: {
+    changeSet(set) {
+      this.currentSet = set === 'Unspecified' ? '' : set;
+      this.cards.forEach(c => c.hide = this.currentSet && !c.set.includes(this.currentSet));
+    }
+  },
+
+  created() {
+    // lazy load images where possible
+    if ('IntersectionObserver' in window) {
+      this.observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) return;
+
           const lazyImage = entry.target;
           lazyImage.src = lazyImage.dataset.src;
           lazyImage.classList.remove('lazy');
-          lazyImageObserver.unobserve(lazyImage);
-        }
-      });
-    }, { threshold: 0.05 });
+          this.observer.unobserve(lazyImage);
+        });
+        
+      }, { root: this.$el, threshold: 0.05 });
+    }
+  },
   
+  mounted() {
+    const lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
+    
     lazyImages.forEach((lazyImage) => {
-      lazyImageObserver.observe(lazyImage);
+      this.observer.observe(lazyImage);
     });
 
-  // load images if no intersectionobserver
-  } else {
-    lazyImages.forEach(img => {
-      img.src = img.dataset.src;
-      img.classList.remove('lazy');
-    });
   }
 });
