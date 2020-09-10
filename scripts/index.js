7bc5c19a0b191efa70080c5e1eae71ce7aeb559c
seiyria <kyle@seiyria.com>
2020-09-10 10:50:52 -0500

    unlazy if io not supported

--- scripts/index.js
+++ scripts/index.js
@@ -2,20 +2,28 @@
 document.addEventListener('DOMContentLoaded', () => {
   const lazyImages = [].slice.call(document.querySelectorAll('img.lazy'));
 
+  // lazy load images where possible
   if ('IntersectionObserver' in window) {
     const lazyImageObserver = new IntersectionObserver((entries, observer) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          let lazyImage = entry.target;
+          const lazyImage = entry.target;
           lazyImage.src = lazyImage.dataset.src;
           lazyImage.classList.remove('lazy');
           lazyImageObserver.unobserve(lazyImage);
         }
       });
-    });
+    }, { threshold: 0.05 });
   
     lazyImages.forEach((lazyImage) => {
       lazyImageObserver.observe(lazyImage);
+    });
+
+  // load images if no intersectionobserver
+  } else {
+    lazyImages.forEach(img => {
+      img.src = img.dataset.src;
+      img.classList.remove('lazy');
     });
   }
 });
